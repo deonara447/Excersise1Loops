304cf6de466a05e74772fe849714aece44c98cef
deonara447 <deonara447@ed.amdsb.ca>
2018-11-19 14:19:38 -0500

    Add project files.

---
+++ Excersise1Loops/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Excersise1Loops
+{
+    public partial class Form1 : Form
+    {
+        int Number1;
+        int Number2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Number1 = Convert.ToInt16(textBox1.Text);
+            Number2 = Convert.ToInt16(textBox2.Text);
+            label1.Text = "";
+
+            while (Number1 <= Number2)
+            {
+                label1.Text += "" + Number1 + " ";
+                Number1++;
+            }
+        }
+    }
+}
